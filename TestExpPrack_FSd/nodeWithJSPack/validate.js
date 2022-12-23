1b4a1d7feba4b1f1ee75b3f51ac595af32a414d9
abhinay5993 <abhinaylunawat5993@gmail.com>
2022-12-24 02:53:18 +0530

    try with hindi tech tutorils + Nametey JS, HTML.

--- TestExpPrack_FSd/nodeWithJSPack/validate.js
+++ TestExpPrack_FSd/nodeWithJSPack/validate.js
@@ -1,12 +1,21 @@
-
 function regValidation() {
     var uname = document.stuRegisterForm.username.value;
     var pwd = document.stuRegisterForm.password.value;
-    console.log("userName value : "+uname);
-    console.log("password value : "+pwd);
-    if (uname == '' || pwd == '') {
-        alert("please enter "+uname+" or "+pwd+" is empty");
+    console.log("userName value : " + uname);
+    console.log("password value : " + pwd);
+    if (uname == "" || pwd == "") {
+        alert("please enter " + uname + " or " + pwd + " is empty");
         return false;
     }
     return true;
+}
+
+function increseCounter() {
+    var cot=0;
+    cot++;
+    document.getElementById("counter").innerHTML = cot;
+}
+
+function resetCounter() {
+    document.getElementById("counter").innerHTML = 0;
 }
