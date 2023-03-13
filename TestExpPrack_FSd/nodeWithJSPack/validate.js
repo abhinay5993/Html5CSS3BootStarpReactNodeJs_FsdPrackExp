5569ffff3e1353b1df247cabcde23e6b8106e9c3
abhinay5993 <abhinaylunawat5993@gmail.com>
2023-03-14 03:23:29 +0530

    adding edubridge packtice HTML table + JS array

--- TestExpPrack_FSd/nodeWithJSPack/validate.js
+++ TestExpPrack_FSd/nodeWithJSPack/validate.js
@@ -1,21 +1,47 @@
-function regValidation() {
-    var uname = document.stuRegisterForm.username.value;
-    var pwd = document.stuRegisterForm.password.value;
-    console.log("userName value : " + uname);
-    console.log("password value : " + pwd);
-    if (uname == "" || pwd == "") {
-        alert("please enter " + uname + " or " + pwd + " is empty");
-        return false;
-    }
-    return true;
-}
+// function regValidation() {
+//     var uname = document.stuRegisterForm.username.value;
+//     var pwd = document.stuRegisterForm.password.value;
+//     console.log("userName value : " + uname);
+//     console.log("password value : " + pwd);
+//     if (uname == "" || pwd == "") {
+//         alert("please enter " + uname + " or " + pwd + " is empty");
+//         return false;
+//     }
+//     return true;
+// }
 
-function increseCounter() {
-    var cot=0;
-    cot++;
-    document.getElementById("counter").innerHTML = cot;
-}
+// function increseCounter() {
+//     var cot=0;
+//     cot++;
+//     document.getElementById("counter").innerHTML = cot;
+// }
 
-function resetCounter() {
-    document.getElementById("counter").innerHTML = 0;
+// function resetCounter() {
+//     document.getElementById("counter").innerHTML = 0;
+// }
+
+// function evenApp(num){
+//     var val=(num%2==0)?"even":"odd";
+//     document.write("<br>Passed input : "+num+" Number is : "+val);
+// }
+
+function salCal(da,hra,basic){
+    let sal=(da+hra+basic)-(basic/10);
+    return sal;
 }
+//document.write("<br>Passed Salary : <br>"+salCal(da,hra,basic));
+
+let datAno=(a,b)=>{
+    return (a*a)+(b*b);
+}
+console.log("\nCall Annonimus func : "+datAno(2,3));
+stu={dateAt:"05-09-1993","test":"mesg Al",app5:"sjjdsjd",datAno:{dat1:"kk",dat2:-304994}};
+console.log("\nGet Object Dta :"+stu.test);
+console.log("\nGet Object Date :"+stu.datAno.dat2);
+
+var stuWithObj=new Object();
+stuWithObj.key1=50
+stuWithObj.key2=-203.0034
+stuWithObj.key3="testing with Object()"
+stuWithObj.key4=-0.99
+console.log("Play with Object() : "+stuWithObj.key4);
