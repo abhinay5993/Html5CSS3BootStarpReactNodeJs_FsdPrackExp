f6fab169f7aef9be8d4da6b5dd7da15e9118b914
abhinay5993 <abhinaylunawat5993@gmail.com>
2023-03-18 00:10:06 +0530

    newly added TS progs for Tuples,2D Arrays,Union operations

--- TestExpPrack_FSd/typeScriptsApps/eduBirdgeExpTypesApp.ts
+++ TestExpPrack_FSd/typeScriptsApps/eduBirdgeExpTypesApp.ts
@@ -5,3 +5,36 @@ function findMaxNum(num1:number, num2:number,num3:number):number
     return maxVal;
 }
 console.log("\nManium out of three : "+findMaxNum(110.399,3,39.884));
+console.log("\nHTML project - https://www.knowledgehut.com/blog/web-development/html-projects");
+var nt:number = 473.3949;
+console.log("****** Play with String Functions *******");
+console.log("\nWorking with toFixed() is : "+nt.toFixed(2));
+console.log("\nWorking with toString() is : "+nt.toString());
+console.log("\nWorking with valueOf() is : "+nt.valueOf());
+console.log("\nWorking with toPrecision() is : "+nt.toPrecision(4));
+var nmDat:string ="  I Heat JS-TS!!...  ";
+console.log("\nWorking with split() is : "+nmDat.split("-"));
+console.log("\nWorking with charAt() is : "+nmDat.charAt(5));
+console.log("\nWorking with indexOf is : "+nmDat.indexOf('T'));
+console.log("\nWorking with replace() is : "+nmDat.replace("JS","JavaScript"));
+console.log("\nWorking with trim() is : "+nmDat.trim());
+console.log("\nWorking with subString() is : "+nmDat.substring(0,7));
+console.log("\nWorking with upperCase() is : "+nmDat.toLowerCase());
+console.log("\nWorking with lowerCase() is : "+nmDat.toUpperCase());
+
+console.log("****** Type Scripts Array *******");
+var arrStr:string[] = ["JS", "psu","Ramu","KK"];
+for(var stItem in arrStr)
+{
+console.log("data Items : "+arrStr[stItem]);
+}
+
+var numMult:number[][]=[[34,45,39],[39,30,20],[39,303,49]];
+for(let i=0;i<numMult.length;i++)
+{
+    for(let j=0;j<numMult[i].length;j++)
+    {
+        console.log(numMult[i][j]);
+    } 
+    console.log();
+}
